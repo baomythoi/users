2ec599098c6d4d5a429ae1947d3f60af4dcebd6b
Kien Tran <tkien.ba@gmail.com>
2025-09-30 14:37:51 +0700

    fix(users): fix get user profile cached bug

--- src/services/user/profile.service.ts
+++ src/services/user/profile.service.ts
@@ -21,12 +21,6 @@ export default new class UserProfile extends BaseService {
 
   async get(params: ProfileParams): Promise<FuncResponse<object>> {
     try {
-      const cacheKey = `users:${params.username}:open-api:profile`;
-      const cachedData = await this.common.redis.getCache(cacheKey);
-
-      if (cachedData)
-        return this.responseSuccess(cachedData);
-
       const detail = await UserReplicaModel.query()
         .alias('user')
         .modify((queryBuilder) => {
@@ -70,7 +64,7 @@ export default new class UserProfile extends BaseService {
         delete detail['password'];
 
       await this.common.redis.addCache({
-        key: cacheKey,
+        key: `users:${params.username}:open-api:profile`,
         value: JSON.stringify(detail)
       }, 60 * 60 * 24 * 30); // Cache valid for 30 days
 
